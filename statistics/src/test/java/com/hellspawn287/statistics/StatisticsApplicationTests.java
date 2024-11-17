ee0cc39d6d55373dd05a9dfe5c07184b3ed67981
Robert Prus <robert.prus87@gmail.com>
2024-11-17 10:16:08 +0100

    Add statistics module

---
+++ statistics/src/test/java/com/hellspawn287/statistics/StatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellspawn287.statistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
